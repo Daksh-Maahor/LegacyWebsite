8e1ba15b10b1305dd5f9ac02d1a20f76d07d889e
Daksh-Maahor <137406623+Daksh-Maahor@users.noreply.github.com>
2024-06-23 10:43:02 +0530

    Add files via upload

---
+++ res/scripts/intro.js
@@ -0,0 +1,29 @@
+const display = document.getElementById("main_text");
+const intro = document.getElementById("intro");
+
+const leftLogo = intro.querySelector(".intro_logo_left_show");
+const rightLogo = intro.querySelector(".intro_logo_right_show");
+
+function max_opacity() {
+    display.style.opacity = '1';
+}
+
+function display_items() {
+    intro.style.display = 'none';
+    display.style.display = 'initial';
+    document.getElementsByTagName('body')[0].style.display = 'block';
+    setTimeout(max_opacity, 100);
+}
+
+function startIntro() {
+    //alert("Now");
+
+    leftLogo.classList.add('intro_logo_left_hide');
+    rightLogo.classList.add('intro_logo_right_hide')
+
+    setTimeout(display_items, 8000);
+}
+
+leftLogo.classList.add('intro_logo_left_show');
+rightLogo.classList.add('intro_logo_right_show');
+setTimeout(startIntro, 5000);
